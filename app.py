34694cc738fcd63470ab11b1b2b23538c612699b
anil050388 <51058962+anil050388@users.noreply.github.com>
2023-05-29 19:23:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ app.layout = html.Div([
 
                 dbc.Col([
                     dbc.Card([
-                        dbc.CardImg(src="/assets/platinum.jpg", top=True),
+                        dbc.CardImg(src="/assets/Platinum.jpg", top=True),
                         html.Div([
                             dbc.CardBody([
                                 html.H3(id='Platinum_Card'),
@@ -112,7 +112,7 @@ app.layout = html.Div([
 
                 dbc.Col([
                     dbc.Card([
-                        dbc.CardImg(src="/assets/palladium.jpg", top=True),
+                        dbc.CardImg(src="/assets/Palladium.jpg", top=True),
                         html.Div([
                             dbc.CardBody([
                                 html.H3(id='palladium_Card'),
